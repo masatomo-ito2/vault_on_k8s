840da2e1fbe3a5f799b9882835e3150617746580
masa3 <masa@hashicorp.com>
2021-02-09 11:00:23 +0900

    minor fix

--- 0.access_k8s_dashboard.sh
+++ 0.access_k8s_dashboard.sh
@@ -34,6 +34,7 @@ EOF
 BEARER_TOKEN=$(kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}")
 
 echo "BEARER TOKEN:"
+echo "================"
 echo ${BEARER_TOKEN}
 
 # Open dashboard in browser
--- 0.helm_repo.sh
+++ 0.helm_repo.sh
@@ -3,3 +3,6 @@
 set -x
 
 helm repo add hashicorp https://helm.releases.hashicorp.com 
+helm repo update
+
+helm search repo hashicorp/vault -l
